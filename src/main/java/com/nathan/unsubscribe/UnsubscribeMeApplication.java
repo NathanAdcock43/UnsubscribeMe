cc92f9b17a3b988ff3e850d53a81f2bb31790a37
NathanAdcock43 <nathan.adcock43@gmail.com>
2024-09-09 14:55:24 -0500

    Added postgres, redis and pgadmin in docker and used a .env to store sensitive values

--- src/main/java/com/nathan/unsubscribe/UnsubscribeMeApplication.java
+++ src/main/java/com/nathan/unsubscribe/UnsubscribeMeApplication.java
@@ -1,11 +1,14 @@
 package com.nathan.unsubscribe;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnsubscribeMeApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(UnsubscribeMeApplication.class, args);
     }
 }
